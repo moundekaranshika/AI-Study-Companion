623b4df44a9c81d978dc73243891258bb4fbbaa9
Anshika Moundekar <moundekaranshika2@gmail.com>
2026-03-31 00:36:31 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,31 +1,26 @@
 import requests
-import os
+import streamlit as st
 
-API_KEY = os.getenv("OPENROUTER_API_KEY")
+API_KEY = st.secrets["OPENROUTER_API_KEY"]
+URL = "https://openrouter.ai/api/v1/chat/completions"
+
 
 def call_ai(prompt):
-    response = requests.post(
-        url="https://openrouter.ai/api/v1/chat/completions",
-        headers={
-            "Authorization": f"Bearer {API_KEY}",
-            "Content-Type": "application/json"
-        },
-        json={
-            "model": "meta-llama/llama-3-8b-instruct",
-            "messages": [{"role": "user", "content": prompt}]
-        }
-    )
-    return response.json()['choices'][0]['message']['content']
+    try:
+        response = requests.post(
+            url=URL,
+            headers={
+                "Authorization": f"Bearer {API_KEY}",
+                "Content-Type": "application/json"
+            },
+            json={
+                "model": "meta-llama/llama-3-8b-instruct",
+                "messages": [{"role": "user", "content": prompt}]
+            }
+        )
 
+        result = response.json()
+        return result['choices'][0]['message']['content']
 
-def generate_summary(text):
-    return call_ai(f"Summarize for a student:\n{text[:3000]}")
-
-def generate_questions(text):
-    return call_ai(f"Generate 5 viva questions with answers:\n{text[:3000]}")
-
-def generate_flashcards(text):
-    return call_ai(f"Create flashcards:\n{text[:3000]}")
-
-def chat_with_notes(text, query):
-    return call_ai(f"Notes:\n{text[:3000]}\nQuestion:\n{query}")
+    except Exception as e:
+        return f"Error: {str(e)}"
